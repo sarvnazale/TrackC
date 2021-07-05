e56687e8a87f08ee9f55a8bf0336693bb2b53f0b
Sarvnaz Alemohammad <sarvenazale@yahoo.com>
2021-07-05 14:59:51 -0400

    added notification dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 void main() {
   runApp(MyApp());
